8f0a6a4d74f504a067aaf6bfa7cbe4fec3021052
Genivaldo2230 <gda_anjos@hotmail.com.br>
2023-12-06 01:14:37 -0300

    add depedencia do thymeleaf 3.4

--- src/main/java/com/example/demo/repository/SeriesRepository.java
+++ src/main/java/com/example/demo/repository/SeriesRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Series;
+import jakarta.validation.ConstraintViolation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeriesRepository extends JpaRepository<Series, Long> {
+
+
+
 
 }
