c311db80e53f5358d6718936e28cef941fff5c1f
Genivaldo2230 <gda_anjos@hotmail.com.br>
2023-12-06 01:19:46 -0300

    Criação e tratamento de erro da pagina addFilmes criação Controller GlobalExceptionHandler.java gerencia a pagina ErroPage fazendo tratamento do erro quando adiconado mais de 4 numero no ano

---
+++ src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String adicionarFilmes( ConstraintViolationException e, @ModelAttribute HttpStatus.Series series, Model model) {
+        List<String> errorMessages = e.getConstraintViolations().stream()
+                .map(constraintViolation -> constraintViolation.getMessage())
+                .collect(Collectors.toList());
+
+        model.addAttribute("errorMessages", errorMessages);
+
+        // Evita salvar no banco se houver erros de validação
+//        return "errorPage";
+        return "adicionarFilme";
+    }
+}
--- src/main/java/com/example/demo/controller/SeriesController.java
+++ src/main/java/com/example/demo/controller/SeriesController.java
@@ -2,12 +2,17 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Series;
 import com.example.demo.repository.SeriesRepository;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class SeriesController {
@@ -15,42 +20,73 @@ public class SeriesController {
     @Autowired
     private SeriesRepository seriesRepository;
 
+    // Rota para mostrar a página inicial
     @GetMapping("/")
     public String mostrarPaginaInicial() {
         return "index";
     }
 
+    // Rota alternativa para mostrar a página inicial
     @GetMapping("/index")
     public String mostrarPaginaIndex() {
-        return "listarSeries";
+        return "listarSeries";  // redireciona para a lista de séries
     }
 
+    // Rota alternativa para mostrar a página inicial em formato HTML
     @GetMapping("/index.html")
     public String mostrarPaginaHtmlIndex() {
         return "index";
     }
 
+    // Rota para exibir o formulário de adição de séries
     @GetMapping("/addSeries")
     public String mostrarFormularioAdicionarSeries(Model model) {
-        model.addAttribute("series", new Series());
+        model.addAttribute("series", new Series());  // Adiciona uma nova instância de Series ao modelo
         return "addSeries";
     }
 
+    // Método que trata requisições POST para a rota "/adicionarFilme"
     @PostMapping("/adicionarFilme")
-    public String adicionarFilme(@ModelAttribute Series series, Model model) {
+    public String adicionarFilme( @ModelAttribute Series series, Model model ) {
+        try {
+            // Lógica para validar os campos do formulário
+            Series.SeriesValidator.validarCampos(series);
+
+            // Lógica para salvar no banco de dados se a validação passar
         seriesRepository.save(series);
 
-        // Retrieve the updated list of series from the database
+            // Recupera todas as séries do banco de dados
         List<Series> lista = seriesRepository.findAll();
 
-        // Add the list of series and a success message to the model
-        model.addAttribute("series", lista);
+            // Adiciona a lista de séries e uma mensagem ao modelo para serem exibidos na view
+            model.addAttribute("series", lista);
         model.addAttribute("mensagem", "Série adicionada com sucesso!");
 
-        // Return the name of the template Thymeleaf to be rendered for the success page
+            // Retorna o nome da página a ser exibida após o sucesso
         return "listarSeries";
+
+        } catch (ConstraintViolationException e) {
+            // Se ocorrer erro de validação, redireciona para a página de erro sem salvar no banco
+            // Adiciona o objeto 'series' ao modelo para ser exibido na página de erro
+            model.addAttribute("series", listarSeries(model));
+
+            // Retorna o nome da página de erro
+            return "errorPage";
+        }
     }
 
+    @GetMapping("/paginaComFormulario")
+    public String mostrarPaginaComFormulario() {
+        return "paginaComFormulario";
+    }
+
+    @GetMapping("/errorPage")
+    public String errorPage() {
+        return "errorPage";
+    }
+
+
+    // Rota para listar todas as séries
     @GetMapping("/listarSeries")
     public String listarSeries(Model model) {
         List<Series> lista = seriesRepository.findAll();
@@ -58,17 +94,30 @@ public class SeriesController {
         return "listarSeries";
     }
 
-    @GetMapping("/voltarParaIndexs")
-    public String voltarParaIndexs() {
+    // Rota para redirecionar para a página inicial
+    @GetMapping("/voltarParaIndex")
+    public String voltarParaIndex() {
         return "redirect:/index";
     }
 
+    // Rota para exibir uma página com vídeo (redireciona para a lista de séries)
     @GetMapping("/paginaComVideo")
     public String paginaComVideo() {
         return "listarSeries";
     }
 
+    // Adicione este método para lidar com a exceção específica
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String handleConstraintViolationException( ConstraintViolationException e, Model model ) {
+        // Extrai as mensagens de erro da exceção
+        List<String> errorMessages = e.getConstraintViolations().stream()
+                .map(constraintViolation -> constraintViolation.getMessage())
+                .collect(Collectors.toList());
 
+        // Adiciona as mensagens de erro ao modelo
+        model.addAttribute("errorMessages", errorMessages);
 
-
+        // Retorna o nome do template Thymeleaf para a página de erro
+        return "errorPage";
+    }
 }
